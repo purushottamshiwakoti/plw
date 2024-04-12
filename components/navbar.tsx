c1bfc613e97a4f29802d496974c12723f015d2ae
puru12 <purushottam.makura@gmail.com>
2024-04-12 21:36:26 +0545

    added navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,58 @@
+import { navBar } from "@/lib/nav";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-navBg text-white h-20 flex items-center justify-between px-[10rem]">
+      <div>
+        <Image
+          src={"/images/logo.svg"}
+          alt="logo"
+          width={180}
+          height={50}
+          priority
+        />
+      </div>
+      <ul className="flex items-center gap-11 ">
+        {navBar.map((item, index) => (
+          <li key={index} className="relative group cursor-pointer">
+            {/* Main content */}
+            <span className="font-semibold text-base">{item.name}</span>
+
+            {/* Underline with transition */}
+            <span className="absolute inset-x-0 bottom-0 h-[1px] bg-white origin-right transition-transform duration-500 transform scale-x-0 group-hover:scale-x-100"></span>
+
+            {item.children && (
+              <div className="opacity-0 absolute left-0 mt-[26px] bg-white w-[12rem] origin-left border-t-blue-700 border-t-[0.2rem]    shadow-md transition-opacity duration-300 group-hover:opacity-100">
+                {item.children.map((child, index) => (
+                  <div
+                    className="relative transition duration-300 hover:bg-red-700 "
+                    key={index}
+                  >
+                    <p className="p-3 text-gray-700 hover:text-white text-base">
+                      {child.name}
+                    </p>
+                    <Separator className="absolute bottom-0" />
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {/* Lower text content */}
+          </li>
+        ))}
+        <div>
+          <Search className="w-5 h-5 cursor-pointer" />
+        </div>
+      </ul>
+      <div>
+        <Button className="bg-buttonBg rounded-[5px] p-[25px] w-[9rem] hover:bg-white hover:text-black uppercase text-[15px] font-[700]">
+          Donations
+        </Button>
+      </div>
+    </nav>
+  );
+};
---
+++ lib/nav.ts
@@ -0,0 +1,76 @@
+export const navBar=[
+   {
+    name:"Home",
+    children:[
+        {
+            name:"Home1",
+          
+        },
+        {
+            name:"Home2",
+          
+        },
+    ]
+
+   },
+   {
+    name:"About",
+    children:[
+        {
+            name:"About1",
+          
+        },
+        {
+            name:"About2",
+          
+        },
+    ]
+    
+   },
+   {
+    name:"Pages",
+    children:[
+        {
+            name:"Pages 1",
+          
+        },
+        {
+            name:"Pages 2",
+          
+        },
+    ]
+   },
+   {
+    name:"Events",
+    children:[
+        {
+            name:"Events 1",
+          
+        },
+        {
+            name:"Events 2",
+          
+        },
+        {
+            name:"Events Detail",
+          
+        },
+    ]
+   },
+   {
+    name:"News",
+    children:[
+        {
+            name:"Classic News",
+          
+        },
+        {
+            name:"NewsDetail",
+          
+        },
+    ]
+   },
+   {
+    name:"Contact"
+   },
+]
