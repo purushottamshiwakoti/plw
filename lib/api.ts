65260211aa1d2901259253382bc5194a7fd6406d
puru12 <purushottam.makura@gmail.com>
2024-04-17 19:42:03 +0545

    cache no store

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,8 @@
 export const apiCall=async(name:string,params:string)=>{
     try {
-        const response=await fetch(`${process.env.APIURL}/${name}?${params}`)
+        const response=await fetch(`${process.env.APIURL}/${name}?${params}`,{
+            cache:"no-store"
+        })
         const res=await response.json();
         return res;
         
