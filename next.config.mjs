d87269fa3eaa2f52f5ea412e7817fa446d6a919d
puru12 <puru.shiw2@gmail.com>
2024-05-09 21:42:45 +0700

    fixed image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1", "93.127.195.143"],
+    domains: ["127.0.0.1", "93.127.195.143", "http://93.127.195.143"],
   },
 };
 
