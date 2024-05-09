32c81f2e378be9e86867ca1c4a7bffbaed10c475
puru12 <puru.shiw2@gmail.com>
2024-05-09 14:52:32 +0700

    working on mobil enav

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,23 +1,37 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
+import { ScrollArea } from "@mantine/core";
 import { DialogClose } from "@radix-ui/react-dialog";
-import { Menu } from "lucide-react";
+import { ChevronDown, ChevronUp, Menu } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+
 export const MobileNav = ({ menu }: { menu: any }) => {
   const path = usePathname();
+  const [openDropdown, setOpenDropdown] = useState<string | null>(null);
+  const [subMenu, setSubMenu] = useState<string[]>([]);
+
+  const toggleDropdown = (title: string) => {
+    setOpenDropdown(openDropdown === title ? null : title);
+  };
+
+  const toggleSubMenu = (item: any) => {
+    console.log(item);
+    setSubMenu((prevSubMenu) => {
+      if (prevSubMenu.includes(item)) {
+        return prevSubMenu.filter((subItem) => subItem !== item);
+      } else {
+        return [...prevSubMenu, item];
+      }
+    });
+  };
+  const renderChildren = (menu: any) => {
+    console.log(menu);
+    return <div>{}</div>;
+  };
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -26,28 +40,149 @@ export const MobileNav = ({ menu }: { menu: any }) => {
         </Button>
       </SheetTrigger>
       <SheetContent>
-        <div className="flex flex-col space-y-3 mt-10">
-          {menu.map(
-            (item: any, index: any) =>
-              item.attributes.children.data.length == 0 && (
-                <DialogClose asChild key={index}>
-                  <Link href={item.attributes.url ?? "/"}>
-                    <span
+        <ScrollArea>
+          <div className="flex flex-col space-y-3 mt-10" key={"D"}>
+            {menu.map((item: any, index: any) => (
+              <div key={index}>
+                {item.attributes.children.data.length === 0 ? (
+                  <DialogClose asChild>
+                    <Link href={item.attributes.url ?? "/"}>
+                      <span
+                        className={cn(
+                          path === item.attributes.url
+                            ? "text-buttonHoverBg font-semibold text-base "
+                            : "font-semibold text-base hover:text-buttonHoverBg "
+                        )}
+                        title={item.attributes.title}
+                      >
+                        {item.attributes.title}
+                      </span>
+                    </Link>
+                  </DialogClose>
+                ) : (
+                  <>
+                    <div
                       className={cn(
                         path === item.attributes.url
                           ? "text-buttonHoverBg font-semibold text-base "
                           : "font-semibold text-base hover:text-buttonHoverBg "
                       )}
-                      // onMouseEnter={() => handleParentClick(item.attributes.title)}
                       title={item.attributes.title}
+                      onClick={() => toggleDropdown(item.attributes.title)}
                     >
-                      {item.attributes.title}
-                    </span>
-                  </Link>
-                </DialogClose>
-              )
-          )}
-        </div>
+                      <span className="flex items-center">
+                        {item.attributes.title}
+                        {openDropdown === item.attributes.title ? (
+                          <ChevronUp className="w-5 h-5 ml-1" />
+                        ) : (
+                          <ChevronDown className="w-5 h-5 ml-1" />
+                        )}
+                      </span>
+                    </div>
+                    <div>
+                      {openDropdown?.includes(item.attributes.title) &&
+                        item.attributes.children.data.map(
+                          (childItem: any, childIndex: any) => {
+                            return (
+                              <div className="flex flex-col mt-2" key={"sn"}>
+                                {childItem.attributes.children.data.length ==
+                                0 ? (
+                                  <DialogClose asChild key={childIndex}>
+                                    <Link
+                                      href={childItem.attributes.url ?? "/"}
+                                    >
+                                      <span
+                                        className={cn(
+                                          path === childItem.attributes.url
+                                            ? "text-buttonHoverBg font-semibold text-base "
+                                            : "font-semibold text-base hover:text-buttonHoverBg "
+                                        )}
+                                        title={childItem.attributes.title}
+                                      >
+                                        {childItem.attributes.title}
+                                      </span>
+                                    </Link>
+                                  </DialogClose>
+                                ) : (
+                                  <>
+                                    <span
+                                      className={cn(
+                                        path === childItem.attributes.url
+                                          ? "text-buttonHoverBg font-semibold text-base "
+                                          : "font-semibold text-base hover:text-buttonHoverBg "
+                                      )}
+                                      title={childItem.attributes.title}
+                                      onClick={() =>
+                                        toggleSubMenu(
+                                          childItem.attributes.title
+                                        )
+                                      }
+                                    >
+                                      <span className="flex items-center">
+                                        {childItem.attributes.title}
+                                        {subMenu.includes(
+                                          childItem.attributes.title
+                                        ) ? (
+                                          <ChevronUp className="w-5 h-5 ml-1" />
+                                        ) : (
+                                          <ChevronDown className="w-5 h-5 ml-1" />
+                                        )}
+                                      </span>
+                                    </span>
+                                    <div>
+                                      {subMenu.includes(
+                                        childItem.attributes.title
+                                      ) &&
+                                        childItem.attributes.children.data.map(
+                                          (item: any, index: number) => (
+                                            <div key={index}>
+                                              {/* {JSON.stringify(item)} */}
+                                              {item.attributes.children.data
+                                                .length == 0 ? (
+                                                <DialogClose
+                                                  asChild
+                                                  key={childIndex}
+                                                >
+                                                  <Link
+                                                    href={
+                                                      item.attributes.url ?? "/"
+                                                    }
+                                                  >
+                                                    <span
+                                                      className={cn(
+                                                        path ===
+                                                          item.attributes.url
+                                                          ? "text-buttonHoverBg font-semibold text-base "
+                                                          : "font-semibold text-base hover:text-buttonHoverBg "
+                                                      )}
+                                                      title={
+                                                        item.attributes.title
+                                                      }
+                                                    >
+                                                      {item.attributes.title}
+                                                    </span>
+                                                  </Link>
+                                                </DialogClose>
+                                              ) : (
+                                                renderChildren(item)
+                                              )}
+                                            </div>
+                                          )
+                                        )}
+                                    </div>
+                                  </>
+                                )}
+                              </div>
+                            );
+                          }
+                        )}
+                    </div>
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
