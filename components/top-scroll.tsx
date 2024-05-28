d79a5fc5d20757808d3c78ef3d6de48c2fbe457d
puru12 <puru.shiw2@gmail.com>
2024-05-28 23:49:30 +0700

    fixed top scroll button

--- components/top-scroll.tsx
+++ components/top-scroll.tsx
@@ -1,7 +1,64 @@
 "use client";
 
-import ScrollToTop from "react-scroll-to-top";
+// import ScrollToTop from "react-scroll-to-top";
+
+// export const TopSctoll = () => {
+//   return <ScrollToTop smooth className="bg-red-500 rounded-full" />;
+// };
+
+import React, { useEffect, useState } from "react";
 
 export const TopSctoll = () => {
-  return <ScrollToTop smooth className="bg-red-500 rounded-full" />;
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 8);
+    }
+  };
+
+  return (
+    <div>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-10 right-10 bg-buttonHoverBg text-white rounded-full w-12 h-12 flex items-center justify-center shadow-lg z-10"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 10l7-7m0 0l7 7m-7-7v18"
+            />
+          </svg>
+        </button>
+      )}
+      {/* Your content here */}
+    </div>
+  );
 };
