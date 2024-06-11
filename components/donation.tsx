8942b06b4c78ee11e7abd7d2b2afbe4b52631115
puru12 <puru.shiw2@gmail.com>
2024-06-11 18:17:37 +0700

    fixed donation

--- components/donation.tsx
+++ components/donation.tsx
@@ -46,25 +46,27 @@ export const Donation = ({
 }: DonationProps) => {
   return (
     <div
-      className="  bg-no-repeat bg-center bg-cover  mb-10"
-      style={{
-        backgroundImage: `url(${AppUrl}${banner})`,
-      }}
+      className="  bg-no-repeat bg-center bg-cover  "
+      // style={{
+      //   backgroundImage: `url(${AppUrl}${banner})`,
+      // }}
     >
-      <div className="bg-[#06B37C]/90 p-10">
+      <div className="bg- p-10">
         {showDonationTitle && (
           <div className="mt-5 space-y-3  ">
-            <h2 className="text-white text-center font-semibold lg:text-3xl text-xl ">
-              {title}
-            </h2>
             <div className="flex gap-[1px]  items-center justify-center ">
-              <Star fill="white" strokeWidth={0} className="w-5 h-5" />
-              <Star fill="white" strokeWidth={0} className="w-7 h-7" />
-              <Star fill="white" strokeWidth={0} className="w-5 h-5" />
+              <Star fill="#F05555" strokeWidth={0} className="w-4 h-4" />
+              <Star fill="#F05555" strokeWidth={0} className="w-6 -mt-3 h-6" />
+              <Star fill="#F05555" strokeWidth={0} className="w-4 h-4" />
             </div>
-            <p className=" text-white lg:text-xl text-lg text-center">
-              {description}
-            </p>
+            <div className="mt-5 s ">
+              <p className="text-[#222] text-center text-[16px] mb-5 mt-3 ">
+                {description}
+              </p>
+              <h2 className="font-[600] lg:text-[40px] md:text-3xl text-2xl text-center text-[#222] mt-2">
+                {title}
+              </h2>
+            </div>
           </div>
         )}
         <div className=" flex justify-center flex-wrap">
@@ -73,11 +75,11 @@ export const Donation = ({
             donations.map((item, index) => {
               return (
                 <div
-                  className="group   hover:-translate-y-5 transition-all duration-500 flex-col lg:my-14 my-5 lg:mx-4 cursor-pointer bg-white/20 rounded-md py-10"
+                  className="group   hover:-translate-y-5 border transition-all duration-500 flex-col lg:my-14 my-5 lg:mx-4 cursor-pointer bg-white/20 rounded-md py-10"
                   key={index}
                 >
                   <div className="  rounded-md  min-w-72 min-h-32 flex flex-col items-center justify-center">
-                    {/* <PersonStandingIcon className="text-white w-24 h-24" /> */}
+                    {/* <PersonStandingIcon className=" w-24 h-24" /> */}
                     {item.Icon !== null && (
                       <Image
                         alt={item.Icon.alt ?? "lll"}
@@ -87,9 +89,11 @@ export const Donation = ({
                       />
                     )}
                   </div>
-                  <div className=" flex flex-col items-center text-white">
-                    <h2 className="font-bold text-3xl">{item.Description}</h2>
-                    <p className="text-white mt-2 text-lg line-clamp-5 ">
+                  <div className=" flex flex-col items-center ">
+                    <h2 className="font-bold text-[#222] text-xl ">
+                      {item.Description}
+                    </h2>
+                    <p className=" mt-1 text-base line-clamp-5 text-[#666]">
                       {item.Title}
                     </p>
                   </div>
@@ -103,13 +107,13 @@ export const Donation = ({
                 opts={{
                   align: "start",
                 }}
-                className="w-full mt-4  lg:px-[5rem] xl:px-[5%]  "
+                className="w-full mt-4  l xl:px-[14%]  "
               >
                 <CarouselContent className="gap-3">
                   {donations &&
                     donations.map((item, index) => (
                       <div
-                        className="group hover:-translate-y-5 transition-all duration-500 flex-col lg:my-14 my-5 lg:mx-4 cursor-pointer bg-white/20 rounded-md py-10"
+                        className="group hover:-translate-y-5 transition-all border duration-500 flex-col lg:my-14 my-5 lg:mx-4 cursor-pointer  rounded-md py-10"
                         key={index}
                       >
                         <div className="rounded-md min-w-72 min-h-32 flex flex-col items-center justify-center">
@@ -122,18 +126,18 @@ export const Donation = ({
                             />
                           )}
                         </div>
-                        <div className="flex flex-col items-center text-white">
-                          <h2 className="font-bold text-3xl">
+                        <div className="flex flex-col items-center ">
+                          <h2 className="font-bold text-[#222] text-xl ">
                             {item.Description}
                           </h2>
-                          <p className="text-white mt-2 text-lg line-clamp-5">
+                          <p className=" mt-1 text-base line-clamp-5 text-[#666]">
                             {item.Title}
                           </p>
                         </div>
                       </div>
                     ))}
                 </CarouselContent>
-                <div className="mt-10">
+                <div className="">
                   <div className="flex items-center justify-center gap-4">
                     <CarouselPrevious className="w-12" />
                     <CarouselNext className="w-12" />
